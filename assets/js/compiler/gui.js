0eb600ad91320c0a8cc8b5a2d85a89b58291dbcb
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-12-02 08:44:06 +0100

    first commit

---
+++ assets/js/compiler/gui.js
@@ -0,0 +1,37 @@
+﻿export class Gui {
+    constructor() {
+    }
+
+    configureEditor() {
+        //EDITOR CODE MIRROR
+        this.editor = CodeMirror.fromTextArea(document.getElementById("source"), {
+            lineNumbers: true,
+            matchBrackets: true,
+            mode: "text/x-csharp"
+        });
+
+        this.output = CodeMirror.fromTextArea(document.getElementById("output"), {
+            lineNumbers: true,
+            matchBrackets: true,
+            readOnly: true,
+            mode: "text/x-csharp"
+        });
+        let mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
+        CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
+    }
+
+    addElementToSelect(item, value) {
+        const select = document.getElementById("optimizationLevel");
+        const option = document.createElement("OPTION"),
+        txt = document.createTextNode(item);
+        option.appendChild(txt);
+        option.setAttribute("value", value);
+        select.insertBefore(option, select.lastChild);
+    }
+
+    hideLoader() {
+        const loader = document.querySelector(".loader");
+        loader.classList.add("loader-hidden");
+        document.body.removeChild("loader");
+    }
+}
