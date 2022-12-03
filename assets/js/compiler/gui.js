4258a7ab8e6f7a7fedb283856b426e3f2f74c12e
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-12-03 09:29:49 +0100

    moving the code for creating the editors

--- assets/js/compiler/gui.js
+++ assets/js/compiler/gui.js
@@ -4,20 +4,8 @@
 
     configureEditor() {
         //EDITOR CODE MIRROR
-        this.editor = CodeMirror.fromTextArea(document.getElementById("source"), {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "text/x-csharp"
-        });
-
-        this.output = CodeMirror.fromTextArea(document.getElementById("output"), {
-            lineNumbers: true,
-            matchBrackets: true,
-            readOnly: true,
-            mode: "text/x-csharp"
-        });
-        let mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
-        CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
+        
+       
     }
 
     addElementToSelect(item, value) {
