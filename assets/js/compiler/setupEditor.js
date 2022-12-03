ccda8f25cc6a9aab19a4e866d079320949508f38
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-12-03 09:50:46 +0100

    moving the creation of editors

---
+++ assets/js/compiler/setupEditor.js
@@ -0,0 +1,19 @@
+﻿const source = `private static void TestKernel(Index1D index, ArrayView<int> input, ArrayView<int> output)
+{
+    output[index] = input[index];
+}`;
+var editor = CodeMirror.fromTextArea(document.getElementById("source"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    mode: "text/x-csharp",
+    extraKeys: { "Ctrl-Space": "autocomplete" }
+});
+
+var output = CodeMirror.fromTextArea(document.getElementById("output"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    readOnly: true,
+    mode: "text/x-csharp",
+    extraKeys: { "Ctrl-Space": "autocomplete" }
+});
+editor.getDoc().setValue(source);
