8dc87bf1dc8c575bf999796318370a7d550dcc11
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-12-05 10:18:52 +0100

    trying to fix the loading of the editor

--- assets/js/compiler/main.js
+++ assets/js/compiler/main.js
@@ -1,6 +1,8 @@
 import { dotnet } from './dotnet.js';
 import { Gui } from './gui.js';
 
+console.log("ciaoo");
+console.log("AA");
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
 
@@ -14,40 +16,45 @@ const source = `private static void TestKernel(Index1D index, ArrayView<int> inp
 {
     output[index] = input[index];
 }`;
+
 var editor = document.querySelectorAll('.CodeMirror')[0].CodeMirror;
 var output = document.querySelectorAll('.CodeMirror')[1].CodeMirror;
 editor.setValue(source);
 editor.refresh();
 
+
 //LOADING OF ASSEMBLIES FOR THE ROSLYN COMPILER
 let totalFiles = 0;
 let arraybuffer;
-const req = new XMLHttpRequest();
-req.responseType = 'json';
-req.open('GET', "mono-config.json", true); //getting the config file that lists all the resources the roslyn compiler needs
-req.onload = function () {
-    var jsonResponse = req.response;
-    arraybuffer = new Array(totalFiles);
-    var loadedFiles = 0;
+function loadDependancies(){
+    const req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open('GET', "../assets/js/compiler/mono-config.json", true); //getting the config file that lists all the resources the roslyn compiler needs
+    req.onload = function () {
+        var jsonResponse = req.response;
+        arraybuffer = new Array(totalFiles);
+        var loadedFiles = 0;
 
-    for (var i = 0; i < jsonResponse.assets.length; i++) {
-        if (jsonResponse.assets[i].behavior == 'assembly' && jsonResponse.assets[i].name.includes(".dll")) {
-            const http = new XMLHttpRequest();
-            http.onload = (e) => {
-                arraybuffer[loadedFiles] = new Uint8Array(http.response);
-                loadedFiles++;
-                if (loadedFiles == totalFiles) { //If i loaded all the files i can enable the compile button
-                   // gui.hideLoader();
-                }
-            };
-            http.open("GET", "./managed/".concat(jsonResponse.assets[i].name));
-            http.responseType = "arraybuffer";
-            http.send();
-            totalFiles++;
+        for (var i = 0; i < jsonResponse.assets.length; i++) {
+            if (jsonResponse.assets[i].behavior == 'assembly' && jsonResponse.assets[i].name.includes(".dll")) {
+                const http = new XMLHttpRequest();
+                http.onload = (e) => {
+                    arraybuffer[loadedFiles] = new Uint8Array(http.response);
+                    loadedFiles++;
+                    if (loadedFiles == totalFiles) { //If i loaded all the files i can enable the compile button
+                       
+                    }
+                };
+                http.open("GET", "../assets/js/compiler/managed/".concat(jsonResponse.assets[i].name));
+                http.responseType = "arraybuffer";
+                http.send();
+                totalFiles++;
+            }
         }
-    }
-};
-req.send(null);
+    };
+    req.send(null);
+}
+
 
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
@@ -71,8 +78,7 @@ setModuleImports("main.js", {
         gui.addElementToSelect(ol, value);
     },
     setOutput: (out) => {
-        output.setValue(out);
-        output.refresh();
+        output.getDoc().setValue(out);
     }
 });
 runMainAndExit(config.mainAssemblyName, []);
